6c3045a5ad1d33a01dc8b5174f984f8b6e3eb941
David Newell <david@danwell>
2015-10-29 19:03:30 +0000

    Clean up licence text

--- sfexaminer.c
+++ sfexaminer.c
@@ -1,4 +1,7 @@
 /*
+ * sfexaminer; a tool for examining character data in Shining Force
+ * Copyright (C) 2015 David Newell
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -9,10 +12,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  */
 
